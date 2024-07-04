342491da4c67e70eec5f2a454931b8271df35c9c
Ayman Nageeb <aimangta123121@gmail.com>
2024-07-04 16:01:00 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <div>
+      <NuxtPage />
+      <span class="mx-12 text-h2 text-error font-weight-bold">Hello</span>
+    </div>
+  </v-app>
+</template>
+
+<style>
+#electron-status {
+  background: #000;
+  color: #fff;
+  position: absolute;
+  font-size: 2rem;
+  font: bold;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  typescript: {
+    shim: false
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  ssr: false,
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
